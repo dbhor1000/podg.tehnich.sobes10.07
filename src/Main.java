db03c13d2664c662bb830ccf2fe34fe977b868b8
dbhor <dbhor1000@gmail.com>
2023-07-10 22:02:48 +0300

    commit to branch

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-
-     //Comment
-
+        testMethod(50);
+        testMethod(200);
+        testMethod(400);
     }
+
+    public static void testMethod(int a) throws Exception1, Exception2 {
+
+        try {
+            if (a >= 100 && a <= 300 ) {
+                System.out.println(a);
+            } else if (a < 100) {
+                throw new Exception1("Exception1 due to the value of a.");
+            }
+            else {
+                throw new Exception1("Exception2 due to the value of a.");
+            }
+
+        } catch (Exception1 | Exception2 e) {
+            System.out.println(e);
+        }
+    }
+
 }
